41ff5e32044aca170e6857a22f86dc34a12bb06d
BSvetlana <svetlanabisacki@gmail.com>
2018-05-11 18:39:41 +0200

    create route

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <router-link :to="{name: 'home'}" class="navbar-brand">Posts</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <router-link :to="{name: 'posts'}" class="nav-link">All Posts<span class="sr-only">(current)</span></router-link>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"></a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Blog Posts
+    </span>
+  </div>
+</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
